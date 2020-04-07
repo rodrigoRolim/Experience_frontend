3566277b3f3af3e360634a8743609ba5ef6991e6
rodrigoRolim <rodrigorolimveras92@gmail.com>
2020-04-07 16:39:46 -0300

    create changepasswordform component

--- src/components/ChangePasswordForm.vue
+++ src/components/ChangePasswordForm.vue
@@ -1,47 +1,161 @@
 <template>
-  <div class="change-pass">
-    <div class="change-pass__message"></div>
-    <div class="inputs">
-      <div class="inputs__last-password">
-        <code-label
-          label="Doces"
-          color="#676a6c"
-          :fontWeight="500"
-          fontSize="1.0rem"
-          fontFamily='"open sans", "Helvetica Neue", Helvetica, Arial, sans-serif'
-        ></code-label>
-        <code-input
-          label="number"
-          placeholder=""
-          :hasIcon="true"
-          name="number"
-          type="password"
-          :width="9"
-          :height="7"
-          required
-          v-model="value"
-        >
-          <template v-slot:icon>
-            <font-awesome-icon icon="password" :style="{ color: '#676a6c' }"></font-awesome-icon>
-          </template>
-        </code-input>
+  <div class="change-password">
+    <div class="header">
+      <div class="header__name">
+        Alterar Senha de acesso
       </div>
-      <div class="inputs__new-password">
-
+      <div class="header__close" @click="closeModal">
+        <font-awesome-icon icon="times" size="lg" color="red"/>
       </div>
     </div>
-  </div>
+    <div class="body">
+      <div class="change-pass__message"></div>
+      <div class="inputs">
+        <div class="inputs__current-password">
+          <code-label
+            label="Senha atual"
+            color="#676a6c"
+            :fontWeight="600"
+            fontSize=".9rem"
+            bind="currentPassword"
+            fontFamily='"open sans", "Helvetica Neue", Helvetica, Arial, sans-serif'
+          ></code-label>
+          <code-input
+            label="currentPassword"
+            placeholder="senha atual"
+            :hasIcon="true"
+            name="currentPassword"
+            type="password"
+            :width="9"
+            :height="7"
+            required
+            v-model="value"
+          >
+            <template v-slot:icon>
+              <font-awesome-icon icon="lock" :style="{ color: '#676a6c' }"></font-awesome-icon>
+            </template>
+          </code-input>
+        </div>
+        <div class="inputs__new-password">
+          <code-label
+            label="Nova senha"
+            color="#676a6c"
+            :fontWeight="600"
+            fontSize=".9rem"
+            bind="newPassword"
+            fontFamily='"open sans", "Helvetica Neue", Helvetica, Arial, sans-serif'
+          ></code-label>
+          <code-input
+            label="newPassword"
+            placeholder="nova senha"
+            :hasIcon="true"
+            name="newPassword"
+            type="password"
+            :width="9"
+            :height="7"
+            required
+            v-model="value"
+          >
+            <template v-slot:icon>
+              <font-awesome-icon icon="lock" :style="{ color: '#676a6c' }"></font-awesome-icon>
+            </template>
+          </code-input>
+        </div>
+        <div class="inputs__confirm-password">
+          <code-label
+            label="Confirmar nova senha"
+            color="#676a6c"
+            :fontWeight="600"
+            fontSize=".9rem"
+            bind="confirmPassword"
+            fontFamily='"open sans", "Helvetica Neue", Helvetica, Arial, sans-serif'
+          ></code-label>
+          <code-input
+            label="confirmPassword"
+            placeholder="Confimar nova senha"
+            :hasIcon="true"
+            name="confirmPassword"
+            type="password"
+            :width="9"
+            :height="7"
+            required
+            v-model="value"
+          >
+            <template v-slot:icon>
+              <font-awesome-icon icon="lock" :style="{ color: '#676a6c' }"></font-awesome-icon>
+            </template>
+          </code-input>
+        </div>
+      </div>
+      <div class="button">
+        <code-button
+          class="button__confirm"
+          padding="9px 0"
+          text="confirmar"
+          bcolor="dodgerblue"
+          color="white"
+        ></code-button>
+      </div>
+    </div>
+  </div>  
 </template>
 <script>
+
 import CodeInput from './base/CodeInput'
 import CodeLabel from './base/CodeLabel'
+import CodeButton from './base/CodeButton'
 export default {
   name: 'ChangePasswordForm',
   components: {
-    CodeInput
+    CodeInput,
+    CodeLabel,
+    CodeButton
+  },
+  
+  computed: {
+
+  },
+  data () {
+    return {
+      show: true,
+      value: ''
+    }
+  },
+  methods: {
+    closeModal () {
+      this.$emit('modal', false)
+    }
   }
 }
 </script>
 <style lang="sass" scoped>
-
+.header__name
+  color: gray
+  align-self: center
+  margin: 0 auto
+.body
+  background-color: white
+  width: 550px
+  padding: 30px 20px
+  border-bottom-left-radius: 4px
+  border-bottom-right-radius: 4px
+.inputs__current-password,
+.inputs__new-password,
+.inputs__confirm-password
+  margin: 20px 0
+.header
+  background-color: white
+  padding: 20px
+  display: flex
+  flex-direction: row
+  justify-content: space-between
+  border-bottom: 1px solid lightgray
+  border-top-left-radius: 4px
+  border-top-right-radius: 4px
+.button
+  display: flex
+  flex-direction: row
+  justify-content: flex-end
+.button__confirm
+  width: 20%  
 </style>
