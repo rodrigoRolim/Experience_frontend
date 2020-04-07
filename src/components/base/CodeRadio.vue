a9e2325da63f09000a357c687ab1379db42b453a
rodrigoRolim <rodrigorolimveras92@gmail.com>
2020-04-07 16:40:29 -0300

    rounded compoent

--- src/components/base/CodeRadio.vue
+++ src/components/base/CodeRadio.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="code-radio">
     <label ref="label" :class="{ 'selected': visible === value, 'unselected': !visible === value }">
-      <i><font-awesome-icon icon="check" size="xs"></font-awesome-icon></i>
+      <i><font-awesome-icon icon="check" size="xs" /></i>
       <input type="radio" :name="name" :id="identifier" @click="confirm" :value="value" v-model="selected"/>
       <span class="label-radio">{{label}}</span>
     </label>
@@ -53,16 +53,15 @@ export default {
   color: #5f5e5e
   font-weight: 600
   font-size: 0.95rem
-  margin-left: 5px
+  margin-left: 15px
 label
   border: 0.5px solid gray
   border-radius: 100%
   color: white
+  width: 20px
+  height: 20px
 label:hover
-  border-width: 1.5px
   cursor: pointer
-label:hover i
-  margin: 5px 3.5px
 .selected i
   visibility: visible
 .unselected i
@@ -74,6 +73,6 @@ label:hover i
 input
   display: none
   margin: 0
-i
-  margin: 7px 4px
+i 
+  margin-left: 3px
 </style>
