c091587060c8a6bea3a0521686016a8d4a2c2614
rodrigoRolim <rodrigorolimveras92@gmail.com>
2020-05-27 15:18:10 -0300

    replace background-color to app div

--- src/App.vue
+++ src/App.vue
@@ -18,7 +18,7 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif
   -webkit-font-smoothing: antialiased
   -moz-osx-font-smoothing: grayscale
-  
+  background-color: #f4f3f37a
 input
   outline: none
 </style>
