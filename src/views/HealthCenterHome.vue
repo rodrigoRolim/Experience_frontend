b0ba034e9d56ceae009065c04542e8fdc7423ad6
rodrigoRolim <rodrigorolimveras92@gmail.com>
2020-06-05 16:35:50 -0300

    create

---
+++ src/views/HealthCenterHome.vue
@@ -0,0 +1,99 @@
+<template>
+  <div class="health-center">
+    <div class="health-center__attendances">
+      <attendance-list :attendances="attendances"/>
+    </div>
+    
+  </div>
+</template>
+
+<script>
+import AttendanceList from '../components/AttendanceList'
+/* import store from '../store' */
+export default {
+  name: 'HealthCenterHome',
+  components: {
+    AttendanceList
+  },
+  data () {
+    return {
+       attendances: [
+        {  
+          photo:"índice.jpeg",
+          name:"Antonio de oliveira veras rolim de moura nogueira silva",
+          age:"33 anos",
+          gender:"masculino",
+          idAttendance:"0/010178",
+          agreement:"particular",
+          dataAttendance:"10/06/2019",
+          dataDelivery:"10/08/2019",
+          exams:"T3 T3L TSH T4 T4L POT SOD T3 T3L TSH T4 T4L POT SOD T3 T3L TSH T4 T4L POT SOD T3 T3L TSH T4 T4L POT SOD",
+          situation:"PF"
+        },
+        {
+          photo:"índice.jpeg",
+          name:"Antonio de oliveira veras",
+          age:"33 anos",
+          gender:"masculino",
+          idAttendance:"0/010178",
+          agreement:"particular",
+          dataAttendance:"10/06/2019",
+          dataDelivery:"10/08/2019",
+          exams:"T3 T3L TSH T4 T4L POT SOD T3 T3L TSH T4 T4L POT SOD T3 T3L TSH T4 T4L POT SOD T3 T3L TSH T4 T4L POT SOD",
+          situation:"TF"
+        },
+        {
+          photo:"índice.jpeg",
+          name:"Antonio de oliveira veras",
+          age:"33 anos",
+          gender:"masculino",
+          idAttendance:"0/010178",
+          agreement:"particular",
+          dataAttendance:"10/06/2019",
+          dataDelivery:"10/08/2019",
+          exams:"T3 T3L TSH T4 T4L POT SOD T3 T3L TSH T4 T4L POT SOD T3 T3L TSH T4 T4L POT SOD T3 T3L TSH T4 T4L POT SOD",
+          situation:"EP"
+        },
+        {
+          photo:"índice.jpeg",
+          name:"Antonio de oliveira veras",
+          age:"33 anos",
+          gender:"masculino",
+          idAttendance:"0/010178",
+          agreement:"particular",
+          dataAttendance:"10/06/2019",
+          dataDelivery:"10/08/2019",
+          exams:"T3 T3L TSH T4 T4L POT SOD T3 T3L TSH T4 T4L POT SOD T3 T3L TSH T4 T4L POT SOD T3 T3L TSH T4 T4L POT SOD",
+          situation:"EA"
+        },
+        {
+          photo:"índice.jpeg",
+          name:"Antonio de oliveira veras",
+          age:"33 anos",
+          gender:"masculino",
+          idAttendance:"0/010178",
+          agreement:"particular",
+          dataAttendance:"10/06/2019",
+          dataDelivery:"10/08/2019",
+          exams:"T3 T3L TSH T4 T4L POT SOD T3 T3L TSH T4 T4L POT SOD T3 T3L TSH T4 T4L POT SOD T3 T3L TSH T4 T4L POT SOD",
+          situation:"NR"
+        }
+      ]
+    }
+  },
+  created () {
+
+  },
+
+}
+</script>
+
+<style lang="sass" scoped>
+.health-center
+  display: flex
+  align-items: center
+  justify-content: center
+.health-center__attendances
+  width: 98%
+
+</style>
