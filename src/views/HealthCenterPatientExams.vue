26ef92b56c7a87ca1c2a04f19f8e4a8ce470c185
rodrigoRolim <rodrigorolimveras92@gmail.com>
2020-06-18 13:42:58 -0300

    create new component

---
+++ src/views/HealthCenterPatientExams.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="health-center-patient-exams">
+    <patient-exams />
+  </div>
+</template>
+
+<script>
+import PatientExams from '../components/PatientExams'
+export default {
+  name: 'HealthCenterPatientExams',
+  components: {
+    PatientExams
+  },
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style lang="sass" scoped>
+.health-center-patient-exams
+  display: flex
+  
+</style>
