8d700a310de70db6188952e39050775e53046003
rodrigoRolim <rodrigorolimveras92@gmail.com>
2020-06-23 16:02:06 -0300

    add sidebar row

--- src/components/TheSidebar.vue
+++ src/components/TheSidebar.vue
@@ -21,6 +21,74 @@
           size="lg"
         ></code-info>
       </div>
+      <div class="sidebar__row">
+        <!-- <strong class="sidebar__row__date">04/07/2016</strong> -->
+        <code-info
+          icon="calendar-alt"
+          info="12/09/2017"
+          description="data atendimento"
+          class="sidebar__date"
+          size="lg"
+        ></code-info>
+        <code-info
+          class="sidebar__exams"
+          icon="flask"
+          info="T3 T3L TSH T4 T4L POT SOD T3 T3L TSH T4 T4L POT SOD T3 T3L TSH T4 T4L POT SOD T3 T3L TSH T4 T4L POT SOD"
+          description="exames"
+          size="lg"
+        ></code-info>
+      </div>
+      <div class="sidebar__row">
+        <!-- <strong class="sidebar__row__date">04/07/2016</strong> -->
+        <code-info
+          icon="calendar-alt"
+          info="12/09/2017"
+          description="data atendimento"
+          class="sidebar__date"
+          size="lg"
+        ></code-info>
+        <code-info
+          class="sidebar__exams"
+          icon="flask"
+          info="T3 T3L TSH T4 T4L POT SOD T3 T3L TSH T4 T4L POT SOD T3 T3L TSH T4 T4L POT SOD T3 T3L TSH T4 T4L POT SOD"
+          description="exames"
+          size="lg"
+        ></code-info>
+      </div>
+      <div class="sidebar__row">
+        <!-- <strong class="sidebar__row__date">04/07/2016</strong> -->
+        <code-info
+          icon="calendar-alt"
+          info="12/09/2017"
+          description="data atendimento"
+          class="sidebar__date"
+          size="lg"
+        ></code-info>
+        <code-info
+          class="sidebar__exams"
+          icon="flask"
+          info="T3 T3L TSH T4 T4L POT SOD T3 T3L TSH T4 T4L POT SOD T3 T3L TSH T4 T4L POT SOD T3 T3L TSH T4 T4L POT SOD"
+          description="exames"
+          size="lg"
+        ></code-info>
+      </div>
+      <div class="sidebar__row">
+        <!-- <strong class="sidebar__row__date">04/07/2016</strong> -->
+        <code-info
+          icon="calendar-alt"
+          info="12/09/2017"
+          description="data atendimento"
+          class="sidebar__date"
+          size="lg"
+        ></code-info>
+        <code-info
+          class="sidebar__exams"
+          icon="flask"
+          info="T3 T3L TSH T4 T4L POT SOD T3 T3L TSH T4 T4L POT SOD T3 T3L TSH T4 T4L POT SOD T3 T3L TSH T4 T4L POT SOD"
+          description="exames"
+          size="lg"
+        ></code-info>
+      </div>
     </div>
   </aside>
 </template>
@@ -35,37 +103,53 @@ export default {
     return {
      
     }
+  },
+  mounted () {
+    
+  },
+  methods: {
+    detectMobile () {
+      return (window.innerWidth <= 768)
+    }
   }
 }
 </script>
 <style lang="sass" scoped>
-
 .sidebar
   display: flex
   flex-direction: column
   background-color: $green
-  height: 100vh
-  width: 320px
+  padding-bottom: 50px
+  @include respond-to(medium-screens)
+    width: 345px
+  @include respond-to(handhelds)
+    width: 100%
   border-right: 1px solid rgba(0,0,0,0.01)
 .sidebar__header
   display: flex
   flex-direction: row
   justify-content: center
   align-items: center
-  background-color: rgba(255,255,255, 0.5)
+  background-color: $green
   height: 12vh
-  color: white 
+  color: white
+  @include respond-to(handhelds)
+    width:100%
 .sidebar__date
   margin-bottom: 20px
 .sidebar__body
   display: flex
   flex-direction: column
-  padding: 10px 0
+  padding-bottom: 20px
 .sidebar__row
   display: flex
   flex-flow: row wrap
   background-color: white
   margin-left: 10px
+  margin-top: 10px
+  @include respond-to(handhelds)
+    margin-right: 10px
+    border-radius: 4px
   color: $green
   padding: 20px 10px
 .sidebar__date,
