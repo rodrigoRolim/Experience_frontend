125f70211c77a4b6d72b311a56fd85510509026e
rodrigoRolim <rodrigorolimveras92@gmail.com>
2020-07-18 19:31:13 -0300

    style(vuex): remove patientExams module

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,12 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import attendances from './modules/attendances'
-import patientExams from './modules/patientExams'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   modules: {
-    attendances,
-    patientExams
+    attendances
   },
   strict: process.env.NODE_ENV !== 'production'
 })
