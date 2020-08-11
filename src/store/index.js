1abf8cc5fe0482334957147cacebeededafddfed
rodrigoRolim <rodrigorolimveras92@gmail.com>
2020-08-11 16:57:01 -0300

    removes

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,13 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import attendances from './modules/attendances'
-
+import auth from './modules/auth'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   modules: {
-    attendances
+    attendances,
+    auth
   },
   strict: process.env.NODE_ENV !== 'production'
 })
