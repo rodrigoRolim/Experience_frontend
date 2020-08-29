0d1c0ee14e52bd786859787c3f045ee13c58666f
rodrigoRolim <rodrigorolimveras92@gmail.com>
2020-08-28 22:01:32 -0300

    create system of alias

--- src/utils/alias.js
+++ src/utils/alias.js
@@ -3,7 +3,7 @@
 */
 
 export const DOCTOR_AUTH = '/token/medico'
-export const PATIENT_AUTH = '/token/paciente'
+export const PATIENT_AUTH = '/token/cliente'
 export const HEALTH_CENTER_AUTH = '/token/posto'
 export const ATTENDANCE_AUTH = '/token/atendimento'
 export const USER_AUTH = '/token/usuario'
@@ -13,56 +13,87 @@ export const USER_AUTH = '/token/usuario'
 */
 
 // doctor
-export const GET_DOCTORS = '/medico'
-export const GET_DOCTOR = (id) => `/medico/${id}`
+export const GET_DOCTORS = '/v1/web/medico'
+export const GET_DOCTOR = (id) => `/v1/web/medico/${id}`
 
 // healthcenter
-export const GET_HEALTH_CENTERS = '/posto'
+export const GET_HEALTH_CENTERS = '/v1/web/posto'
 
 // attendances
-export const GET_ATTENDANCES_HEALTH_CENTER = (healthCenter, attendance) => `/atendimento/${healthCenter}/${attendance}`
-export const GET_ATTENDANCES_DATES_HEALTH_CENTER = (healthCenter, begin, end) => `/atendimento/${begin}/${end}/posto/${healthCenter}`
-export const GET_ATTENDANCES_DATES_REQUESTER = (requester, begin, end) => `/atendimento/${begin}/${end}/solicitante/${requester}`
-export const GET_ATTENDANCES_DATES_CLIENT = (client, begin, end) => `/atendimento/${begin}/${end}/cliente/${client}`
+export const GET_ATTENDANCES = (healthCenter, attendance) => `/v1/web/atendimento/${healthCenter}/${attendance}`
+export const GET_ATTENDANCES_HEALTH_CENTER = (healthCenter, begin, end) => `/v1/web/atendimento/${begin}/${end}/posto/${healthCenter}`
+export const GET_ATTENDANCES_REQUESTER = (requester, begin, end) => `/v1/web/atendimento/${begin}/${end}/solicitante/${requester}`
+export const GET_ATTENDANCES_PATIENT = (client, begin, end) => `/v1/web/atendimento/${begin}/${end}/cliente/${client}`
 
 // agreements medical
-export const GET_AGREEMENTS = '/convenio'
-export const GET_AGREEMENT = (agreement) => `/convenio/${agreement}`
+export const GET_AGREEMENTS = '/v1/web/convenio'
+export const GET_AGREEMENT = (agreement) => `/v1/web/convenio/${agreement}`
 
 // procediments
-export const GET_PROCEDIMENT = (mnemonico) => `/procedimento/${mnemonico}`
-export const GET_PROCEDIMENT_LIKE = (mnemonico) => `/procedimentolike/${mnemonico}`
+export const GET_PROCEDIMENT = (mnemonico) => `/v1/web/procedimento/${mnemonico}`
+export const GET_PROCEDIMENT_LIKE = (mnemonico) => `/v1/web/procedimentolike/${mnemonico}`
 
 // exams
-export const GET_EXAMS_ATTENDANCE = (healthCenter, attendance) => `/exame/${healthCenter}/${attendance}`
+export const GET_EXAMS_ATTENDANCE = (healthCenter, attendance) => `/v1/web/exame/${healthCenter}/${attendance}`
 
 // results
-export const GET_RESULTS = (healthCenter, attendance) => `/resultado/${healthCenter}/${attendance}`
-
+export const GET_RESULTS = (healthCenter, attendance) => `/v1/web/resultado/${healthCenter}/${attendance}`
+/**  
+ * ROUTERS OF PAGE
+*/
+export const PROCEDIMENT_ROUTE = '/manuais'
+export const DOCTOR_ROUTE = '/medico'
+export const DOCTOR_PATIENT_ROUTE = (patient, attendance) => `/medico/paciente/${patient}/${attendance}`
+export const ACESS_ROUTE = '/acesso'
+export const PARTNER_ROUTE = '/parceiro'
+export const PARTNER_PATIENT_ROUTE = (patient, attendance) => `/parceiro/${patient}/${attendance}`
+export const PATIENT_ROUTE = '/paciente'
+export const HEALTH_CENTER_ROUTE = '/posto'
+export const HEALTH_CENTER_PATIENT_ROUTE = (patient, attendance) => `/paciente/${patient}/${attendance}`
 /**  
  * USER TYPES
 */
 export const DOCTOR_TYPE = 0
-export const PATIENT_TYPE = 1
+export const PATIENT_TYPE = 1 // acessp paciente com cpf
 export const HEALTH_CENTER_TYPE = 2
-export const USER_TYPE = 3
-export const ATTENDANCE_TYPE = 4
+export const USER_TYPE = 3 //qrcode
+export const ATTENDANCE_TYPE = 4 // acesso paciente com atendimento
+export const PARTNER_TYPE = 5
 /**  
  * MUTATIONS TYPES AND ACTIONS' NAMES 
 */
 export const AUTH_REQUEST = 'AUTH_REQUEST'
 export const AUTH_SUCCESS = 'AUTH_SUCCESS'
 export const AUTH_ERROR = 'AUTH_ERROR'
+export const AUTH_REINIT_STATUS = 'AUTH_REINIT_STATUS'
 export const AUTH_LOGOUT = 'AUTH_LOGOUT'
+export const GET_NAME_PATIENT = 'GET_NAME_PATIENT'
+export const GET_GENDER_PATIENT = 'GET_GENDER_PATIENT'
+export const GET_AGE_PATIENT = 'GET_AGE_PATIENT'
 /**  
  * MUTATIONS TYPES AND ACTIONS' NAMES along namespace
 */
-export const AUTH_REQUEST_NAMESPACED = 'auth/AUTH_REQUEST'
-export const AUTH_SUCCESS_NAMESPACED = 'auth/AUTH_SUCCESS'
-export const AUTH_ERROR_NAMESPACED = 'auth/AUTH_ERROR'
-export const AUTH_LOGOUT_NAMESPACED = 'auth/AUTH_LOGOUT'
+export const NAMESPACED_AUTH = 'auth'
+export const NAMESPACED_PATIENT = 'patient'
+
 
 /**  
  * VALIDATIONS MESSAGES
 */
 export const REQUIRED_INPUT = 'campo obrigatório'
+export const INCOMPLETE_ID = 'ID incompleto'
+export const INCOMPLETE_CPF = 'cpf incompleto'
+export const INVALID_DATA = 'data inválida'
+export const INCOMPLET_CRM = 'crm incompleto'
+export const MESSAGE_STATUS_403 = 'dados incorretos, verifique seus dados e tente novamente'
+export const MESSAGE_STATUS_111 = 'preencha ou corrija os campos abaixo'
+export const MESSAGE_STATUS_401 = 'dados incorretos, verifique seus dados e tente novamente'
+export const MESSAGE_STATUS_404 = (data) => `${data}(s) não encontrado(s)`
+export const MESSAGE_STATUS_502 = 'conexão recusada, servidor pode está desativado'
+/**
+ * VALIDATORS REGEX
+ * 
+ * **/
+//eslint-disable-next-line
+export const CPF_VALIDATOR = /[0-9]{3}\s\.\s[0-9]{3}\s\.\s[0-9]{3}\s\-\s[0-9]{2}$/g
+export const DATE_VALIDATOR = /[0-9]{2}\s\/\s[0-9]{2}\s\/\s[0-9]{4}/g
