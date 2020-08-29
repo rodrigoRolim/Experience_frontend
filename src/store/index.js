7f373492a591256770ba7ede82675e308b9db61c
rodrigoRolim <rodrigorolimveras92@gmail.com>
2020-08-28 22:01:07 -0300

    create patient store

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,14 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import attendances from './modules/attendances'
+import patient from './modules/patient'
 import auth from './modules/auth'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   modules: {
     attendances,
+    patient,
     auth
   },
   strict: process.env.NODE_ENV !== 'production'
