a1a64a150105c41bc15b7e7698b98c786d458a3b
rodrigoRolim <rodrigorolimveras92@gmail.com>
2020-08-28 22:01:41 -0300

    styles

--- src/views/DoctorPatientExams.vue
+++ src/views/DoctorPatientExams.vue
@@ -75,7 +75,7 @@ export default {
     width: 100%
 .doctor-patient__exams
   width:  calc( 100% - 321px )
-  margin-top: 110px
+  margin-top: 90px
   @include respond-to(medium-screens)
     width: 100%
     margin-top: 60px
