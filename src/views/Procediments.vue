c9c269ac0b117fb656ddf3c80e5df5ae7a1a138f
rodrigoRolim <rodrigorolimveras92@gmail.com>
2020-08-28 22:02:07 -0300

    styles

--- src/views/Procediments.vue
+++ src/views/Procediments.vue
@@ -69,7 +69,7 @@ export default {
 .procediments__sign
   margin-right: 15px
 .procediments__list
-  margin-top: 30px
+  margin-top: 20px
   @include respond-to(handhelds)
-    margin-top: 70px
+    margin-top: 60px
 </style>
