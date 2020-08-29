89bf526a69c01d0eab34147d71eef39e281ae457
rodrigoRolim <rodrigorolimveras92@gmail.com>
2020-08-28 22:00:25 -0300

    create system of messages

---
+++ src/mixins/user-messages/index.js
@@ -0,0 +1,19 @@
+import { MESSAGE_STATUS_111, MESSAGE_STATUS_502, MESSAGE_STATUS_403, MESSAGE_STATUS_401, MESSAGE_STATUS_404 } from '../../utils/alias'
+export const messages = {
+  methods: {
+    message (status, data = 'recurso') {
+      switch (status) {
+        case 111:
+          return { type: 'error', message: MESSAGE_STATUS_111 }
+        case 403: 
+          return { type: 'error', message: MESSAGE_STATUS_403 }
+        case 404:
+          return { type: 'error', message: MESSAGE_STATUS_404(data) }
+        case 401: 
+          return { type: 'error', message: MESSAGE_STATUS_401 }
+        case 502:
+          return { type: 'error', message: MESSAGE_STATUS_502}
+      }
+    }
+  }
+}
