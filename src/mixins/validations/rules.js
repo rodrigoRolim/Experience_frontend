364835f475a21074ba489c3901b35e3b32a25cc8
rodrigoRolim <rodrigorolimveras92@gmail.com>
2020-08-28 22:00:37 -0300

    create validations

--- src/mixins/validations/rules.js
+++ src/mixins/validations/rules.js
@@ -2,7 +2,7 @@ export const required = function (value) {
   return value == ''
 }
 export const min = function (value, min) {
-  return value.length < min
+  return value?.length < min
 }
 export const max = function (value, max) {
   return value.length < max
@@ -13,9 +13,19 @@ export const email = function (value, regex) {
 export const password = function (value, regex) {
   return regex.test(value)
 }
-export const dateFormat = function (value, regex) {
-  return regex.test(value)
+export const date = function (value, regex) {
+  return !regex.test(value)
+}
+export const cpfValide = function (value, regex) {
+  return !regex.test(value)
 }
 export const compareDates = function (begin, end) {
   return begin <= end
+}
+export const validateForm = function (form, validate, message) {
+  let fields = Object.keys(form).filter(el => form[el] == '' || form[el] == '')
+  fields.forEach(element => {
+    validate[element] = message
+  })
+  return fields.length > 0
 }
