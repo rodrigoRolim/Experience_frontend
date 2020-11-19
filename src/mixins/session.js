65a410727df85ec19e3aba8b6c12b3012129b90a
rodrigoRolim <rodrigorolimveras92@gmail.com>
2020-11-19 12:07:04 -0300

    create mixin session

---
+++ src/mixins/session.js
@@ -0,0 +1,21 @@
+import { DOCTOR_TYPE, PATIENT_TYPE, HEALTH_CENTER_TYPE, USER_TYPE, ATTENDANCE_TYPE, PARTNER_TYPE } from '../utils/alias'
+export const session = {
+  methods: {
+    getTypeUser (typeUser) {
+      switch (typeUser) {
+        case DOCTOR_TYPE:
+          return 'medico'
+        case PATIENT_TYPE:
+          return 'cliente'
+        case HEALTH_CENTER_TYPE:
+          return 'posto'
+        case USER_TYPE:
+          return 'cliente'
+        case ATTENDANCE_TYPE:
+          return 'cliente'
+        case PARTNER_TYPE:
+          return 'parceiro'
+      }
+    }
+  }
+}
