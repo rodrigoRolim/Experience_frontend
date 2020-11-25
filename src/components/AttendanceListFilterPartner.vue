c92511adc526f793f850579c7cdf8e343231b518
rodrigoRolim <rodrigorolimveras92@gmail.com>
2020-11-25 12:48:44 -0300

    implement request of attendances from api

--- src/components/AttendanceListFilterPartner.vue
+++ src/components/AttendanceListFilterPartner.vue
@@ -13,9 +13,13 @@
           />
           <div class="attendances-filter-partner__calendars">
             <attendance-list-filter-period 
-            :begin="'20-09-20'"
-            :end="'01-10-20'"
-            v-model="period"/>
+             :begin="params.begin"
+             :end="params.end"
+             @begin="setBegin"
+             @end="setEnd"
+             :error-begin="validate.begin"
+             :error-end="validate.end"
+            />
           </div>
         </div>
         <div class="attendances-filter-partner__accomodation">
@@ -31,7 +35,10 @@
             class="attendances-filter-partner__select"
             name="acomodations"
             option="selecione a acomodação"
-            :options="list"
+            :options="accomodations"
+            @input="setAccomodation"
+            :value="params.accomodation"
+            :error="validate.accomodation"
           />
         </div>
         <div class="attendances-filter-partner__situation">
@@ -47,9 +54,10 @@
             class="attendances-filter-partner__select"
             name="situation"
             option="selecione a situação"
-            :options="list"
-            :width="9"
-            :height="8" 
+            :options="situations"
+            @input="setSituation"
+            :value="params.situation"
+            :error="validate.situation"
           />
         </div>
         <div class="attendances-filter-partner__patient-name">
@@ -79,6 +87,9 @@
             size="md"
             streched
             size-icon="lg"
+            :disable="disableConfirm"
+            :loading="disableConfirm"
+            @click.prevent="confirm"
           />
         </div>
       </div>
@@ -93,8 +104,34 @@ import CodeSelect from '../components/base/CodeSelect'
 import CodeInput from '../components/base/CodeInput'
 import CodeButton from '../components/base/CodeButton'
 import AttendanceListFilterPeriod from '../components/AttendanceListFilterPeriod'
+import { validator } from '../mixins/validations/validator'
+import { session } from '../mixins/session'
+import { isOption, ltBegin, gtEnd, required, date } from '../mixins/validations/rules'
+import { messages } from '../mixins/user-messages'
+import { mapActions, mapGetters, mapMutations } from 'vuex'
+import { 
+  NAMESPACED_ATTENDANCE, 
+  NAMESPACED_AUTH,
+  GET_ATTENDANCES, 
+  GET_ATTENDANCES_STORE,
+  GET_ACCOMODATIONS_STORE,
+  NAMESPACED_ACCOMODATIONS,
+  GET_FILTERS, 
+  ATTENDANCE_NOT_FOUND,
+  ACCOMODATIONS,
+  SITUATIONS, 
+  BEGIN_DATE,
+  END_DATE,
+  ACCOMODATION,
+  SITUATION,
+  DEFAULT_DATES,
+  DATE_VALIDATOR,
+  REINIT_PAGINATION,
+  EMPTY_ATTENDANCES
+} from '../utils/alias'
 export default {
   name: 'AttendanceListFilterPartner',
+  mixins: [messages, validator({ isOption, ltBegin, gtEnd, required, date }), session],
   components: {
     CodeDropDown,
     CodeLabel,
@@ -106,8 +143,208 @@ export default {
   data () {
     return {
       list: [],
-      period: null
+      period: null,
+      situations: SITUATIONS,
+      validate: {
+        accomodation: '',
+        situation: '',
+        begin: '',
+        end: ''
+      },
     }
+  },
+   computed: {
+    ...mapGetters(NAMESPACED_ATTENDANCE, [
+      'params',
+      'status'
+    ]),
+    ...mapGetters(NAMESPACED_AUTH, [
+      'userId',
+      'userTypeAuthed',
+    ]),
+    ...mapGetters(NAMESPACED_ACCOMODATIONS, {
+      accomodations: 'accomodations',
+      statusAcc: 'status'
+    }),
+    allowRequest () {
+      return !Object.values(this.validate).find((val) => val !== '')
+    },
+    beginAndEnd () {
+      return `${this.params.begin}|${this.params.end}`
+    },
+    disableConfirm () {
+      return this.status == 'loading' || this.statusAcc == 'loading' 
+    }
+  },
+  watch: {
+    'params.begin': function (value) {
+      if (this.required(value)) {
+
+        this.validate.begin = 'campo obrigatório'
+      } else if (this.gtEnd(value, this.params.end)) {
+        
+        this.validate.begin = 'data inicial inválida'
+      } else if (this.ltBegin(this.params.end, this.params.begin)){
+        
+        this.validate.end = 'data final inválida'
+      } else {
+        this.validate.begin = ''
+        this.validate.end = ''
+      }
+    },
+    'params.end': function (value) {
+      if (this.required(value)) {
+        this.validate.end = 'campo obrigatório'
+
+      } else if (this.ltBegin(value, this.params.begin)) {
+        this.validate.end = 'data final inválida'
+      } else if (this.gtEnd(this.params.begin, this.params.end)){
+        this.validate.begin = 'início inválido'
+      } else {
+        this.validate.begin = ''
+        this.validate.end = ''
+      }
+    },
+    'params.accomodation': function (value) {
+
+      if (this.required(value)) {
+        this.validate.accomodation = 'campo obrigatório'
+      } else if (this.isOption(value, this.accomodations)) {
+        this.validate.accomodation = 'opção inválida'
+      } else {
+        this.validate.accomodation = ''
+      }
+    },
+    'params.situation': function (value) {
+      if (this.required(value)) {
+        this.validate.situation = 'campo obrigatório'
+      } else if (this.isOption(value, this.situations)) {
+        this.validate.situation = 'opção inválida'
+      } else {
+        this.validate.situation = ''
+      }
+    },
+    waitRequest () {
+      return this.statusAcc == 'loading' || this.status == 'loading'
+    },
+    beginAndEnd (value) {
+      let [begin, end] = value.split('|')
+      if (this.date(begin, DATE_VALIDATOR) && this.date(end, DATE_VALIDATOR)) {
+        // this.backParamsToDefault()
+        this.initComponent()
+      }
+    }
+  },
+  methods: {
+     initFilters () {
+      this.filters = Object.assign({}, this.params)
+    },
+    backParamsToDefault () {
+      const defaultOption = {id: '', name: 'todos'}
+      this.setHealthCenter(defaultOption)
+      this.setAccomodation(defaultOption)
+      this.setRealizer(defaultOption)
+    },
+    async initComponent () {
+
+      try {
+        await this.listAccomodations()
+        await this.attendances()
+      } catch (err) {
+        console.log({err})
+        this.setMessage(this.message({ status: err.response.status, data: 'atendimento' }))
+      }
+    },
+    catchErrorSelect (error) {
+      if (!error) {
+        this.requestPrevent = true
+      }
+    },
+    async confirm () {
+
+      if (this.allowRequest) {
+        try {
+          await this.attendances()
+        } catch (err) {
+           this.setMessage(this.message({ status: err.response.status, data: 'atendimento' }))
+        }
+      } else {
+        this.$emit("error", this.message({ status: 111 }))
+      }
+    },
+    getURI(id, typeUser, resource) {
+      return GET_FILTERS(
+              this.params.begin.split(" - ").join("-"), 
+              this.params.end.split(" - ").join("-"), 
+              typeUser,
+              id, 
+              resource
+            )
+    },
+    listAccomodations () {
+      return new Promise((resolve, reject) => {
+        let urlAccomodations = this.getURI(this.userId, this.getTypeUser(this.userTypeAuthed), ACCOMODATIONS)
+        this.getAccomodations({ url: urlAccomodations })
+          .then((accomodations) => {
+            resolve(accomodations)     
+          })
+          .catch((err) => {
+            console.log(err)
+            reject(err)
+          })
+      })
+    },
+     paramsQuery () {
+      let queries = {}
+      if (this.params.healthCenter.id) queries['postocadastro'] = this.params.healthCenter.id
+      if (this.params.realizer.id) queries['postorealizante'] = this.params.realizer.id
+      if (this.params.accomodation.id) queries['acomodacao'] = this.params.accomodation.id
+      if (this.params.situation.id) queries['situacao'] = this.params.situation.id
+      queries['limit'] = this.params.limit
+      queries['page'] = this.params.page
+      return queries
+    },
+    attendances () {
+      if (!this.waitRequest) {
+        let headers = { 'X-Paginate': true }
+        return new Promise((resolve, reject) => {
+          let healthCenter = this.userId
+  
+          this.renitiPage()
+          //console.log("page: ", this.params.page)
+          let urlName = GET_ATTENDANCES(
+            healthCenter,
+            this.params.begin.split(" - ").join("-"),
+            this.params.end.split(" - ").join("-"),
+            this.getTypeUser(this.userTypeAuthed))   
+          this.getAttendances({ url: urlName, params: this.paramsQuery(), headers: headers })
+          .then((res) => {
+            
+            resolve(res)
+          })
+          .catch((err) => {
+            console.log({err})
+            reject(err)
+          })
+        })
+      }
+    },
+    ...mapMutations(NAMESPACED_ATTENDANCE, {
+      setBegin: BEGIN_DATE,
+      setEnd: END_DATE,
+      setAccomodation: ACCOMODATION,
+      setSituation: SITUATION,
+      setInitialDates: DEFAULT_DATES,
+      renitiPage: REINIT_PAGINATION,
+      emptyAttendances: EMPTY_ATTENDANCES
+    }),
+    ...mapActions(NAMESPACED_ATTENDANCE, {
+      getAttendances: GET_ATTENDANCES_STORE,
+      setMessage: ATTENDANCE_NOT_FOUND
+    }),
+    ...mapActions(NAMESPACED_ACCOMODATIONS, {
+      getAccomodations: GET_ACCOMODATIONS_STORE
+    })
   }
 }
 </script>
@@ -145,7 +382,8 @@ export default {
     margin: 5px 0
 .attendances-filter-partner__buttons
   width: 10%
-  align-self: flex-end
+  align-self: center
+  margin-top: 10px
   @include respond-to(medium-screens)
     width: 100%
     margin-top: 30px
