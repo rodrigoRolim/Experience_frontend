fef5fb18cfea3a84b736d692e56d82cf24233b65
rodrigoRolim <rodrigorolimveras92@gmail.com>
2020-12-31 14:27:37 -0300

    add service for request pdfs

--- src/services/api.js
+++ src/services/api.js
@@ -1,4 +1,4 @@
-import { serverExperience, serverAuth } from '../config/axios'
+import { serverExperience, serverAuth, serverPDF } from '../config/axios'
 import axios from 'axios'
 export const requestResource = ({ url = '', params, data = {}, method = 'GET', headers = {} }) => {
   console.log(headers)
@@ -17,4 +17,11 @@ export const multipleRequests = (cbs = []) => {
 export const requestToken = ({ url, auth }) => {
 
   return serverAuth.post(url, auth)
+}
+export const requestPDF = ({ url, params, method = 'GET' }) => {
+  return serverPDF({
+    url,
+    params,
+    method
+  })
 }
