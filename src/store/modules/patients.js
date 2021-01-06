749f97efaa178611d366761ea62a45c3d60b7b37
rodrigoRolim <rodrigorolimveras92@gmail.com>
2021-01-06 17:11:39 -0300

    style

--- src/store/modules/patients.js
+++ src/store/modules/patients.js
@@ -38,7 +38,8 @@ let begin = () => {
   let today = new Date()
   today.setDate(today.getDate() - 7)
   let currDate = (today.getDate() < 10) ? '0' + today.getDate() : today.getDate()
-  let currMonth = today.getMonth() + 1
+  let month = today.getMonth() + 1
+  let currMonth = (month < 10) ? '0' + month : month
   let currYear = today.getFullYear()
   let dateInitial = currDate + ' / ' + currMonth + ' / ' + currYear
   return dateInitial
@@ -47,7 +48,8 @@ let end = () => {
   let today = new Date()
 
   let nextDate = (today.getDate() < 10) ? '0' + today.getDate() : today.getDate()
-  let nextMonth = today.getMonth() + 1
+  let month = today.getMonth() + 1
+  let nextMonth = (month < 10) ? '0' + month : month
   let nextYear = today.getFullYear()
   let dateFinal = nextDate + ' / ' + nextMonth + ' / ' + nextYear
   return dateFinal
