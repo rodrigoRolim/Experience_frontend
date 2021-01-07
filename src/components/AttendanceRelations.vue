fb95ed0083fa27bfdf68e56fe707dda86cecd256
rodrigoRolim <rodrigorolimveras92@gmail.com>
2021-01-07 19:44:56 -0300

    pass props to attendance relations item

--- src/components/AttendanceRelations.vue
+++ src/components/AttendanceRelations.vue
@@ -1,79 +1,74 @@
 <template>
   <div class="attendance-relations">
     <div class="attendance-relations__item"
-      v-for="attendance in attendances"
-      :key="attendance.correl"
-      :class="{ 'attendance-relations__item--active': setIdentifier(attendance.posto, attendance.atendimento) === selected }"
+      v-for="(attendance, i) in attendances"
+      :key="attendance.atendimento"
+      :class="{ 
+        'attendance-relations__item--active': setIdentifier(attendance.posto, attendance.atendimento) === selected,
+      }"
       @click="select(attendance.posto, attendance.atendimento)"
     >
       <attendance-relations-item
-        :attendance-date="attendance.data_atd | data"
+        :item="i"
+        :name-patient="attendance.nome_cliente"
+        :delivery-date="attendance.data_entrega | date"
+        :gender="attendance.sexo | sex"
+        :age="attendance.data_nas | age"
+        :doctor="attendance.nome_solicitante"
+        :attendance-date="attendance.data_atd | date"
         :attendance="attendance.atendimento"
-        :health-center="healthCenter"
+        :health-center="attendance.posto"
         :list-exams="attendance.mnemonicos"
+        @selected="selectedByDefault"
       />
     </div>
   </div>
 </template>
 <script>
 import AttendanceRelationsItem from './AttendanceRelationsItem'
-import { mapGetters } from 'vuex'
-import { NAMESPACED_ATTENDANCE, NAMESPACED_PROPS }  from '../utils/alias'
-
+import { mapGetters, mapMutations } from 'vuex'
+import { NAMESPACED_ATTENDANCE, NAMESPACED_EXAMS, SELECTED }  from '../utils/alias'
+import { formater } from '../mixins/formater'
 export default {
   name: 'AttendanceRelations',
   components: {
     AttendanceRelationsItem
   },
+  mixins: [formater],
   data () {
     return {
       selected: ''
     }
   },
   created() {
-    this.selected = this.initialySelected
+    //this.selected = this.setIdentifier(this.healthCenter, this.attendance)
+    //console.log(this.selected)
   }, 
-  filters: {
-    data(dateString) {
-      const date = new Date(dateString).toLocaleDateString("pt-BR")
-      return date !== "Invalid Date" ? date : ""
-    },
-    age(dateString) {
-      var today = new Date();
-      var birthDate = new Date(dateString);
-
-      var age = today.getFullYear() - birthDate.getFullYear();
-      var m = today.getMonth() - birthDate.getMonth();
-      if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
-        age--;
-      }
-      const ageDate = age.toString()
-
-      return !isNaN(ageDate) ? ageDate : "";
-    },
-    id(healthCenter, attendanceId) {
-      return healthCenter + '/' + attendanceId
-    }
-  },
   computed: {
     ...mapGetters(NAMESPACED_ATTENDANCE, [
       'attendances'
-    ]),
-    ...mapGetters(NAMESPACED_PROPS, [
-      'healthCenter',
-      'attendance'
-    ]),
-    initialySelected() {
-      return this.healthCenter + '/' + this.attendance
-    }
+    ])
   },
   methods: {
+    ...mapMutations(NAMESPACED_EXAMS, {
+      selectedToRequestExams: SELECTED
+    }),
     setIdentifier(healthCenter, attendanceId) {
       return healthCenter + '/' + attendanceId
     },
     select(healthCenter, attendanceId) {
       this.selected = this.setIdentifier(healthCenter, attendanceId)
-    }
+      this.startExamsLoading(healthCenter, attendanceId)
+    },
+    startExamsLoading(hc, att) {
+      this.selectedToRequestExams({healthCenter: hc, attendance: att})
+    },
+    selectedByDefault(value) {
+      console.log(value)
+      this.selected = this.setIdentifier(value.healthCenter, value.attendance)
+      this.startExamsLoading(value.healthCenter, value.attendance)
+    },
+    
   }
 }
 </script>
@@ -89,7 +84,8 @@ export default {
   cursor: pointer
   user-select: none
   max-width: 350px
-.attendance-relations__item--active
+.attendance-relations__item--active,
+.attendance-relations__item--initial-active
   background-color: white
   width: 305px
   color: $green
