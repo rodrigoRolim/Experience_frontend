f88c1bb96e815fb0db3418fbc50fe9f59c396ebf
rodrigoRolim <rodrigorolimveras92@gmail.com>
2021-01-13 20:54:03 -0300

    refactor formaters

--- src/mixins/formater.js
+++ src/mixins/formater.js
@@ -1,6 +1,6 @@
-export const formater = {
+export const age = {
   filters: {
-    age (dateString) {
+    ageByBirthday (dateString) {
       var today = new Date();
       var birthDate = new Date(dateString);
       var age = today.getFullYear() - birthDate.getFullYear();
@@ -10,18 +10,85 @@ export const formater = {
       }
       const ageDate = age.toString()
       return !isNaN(ageDate) ? ageDate : "";
-    },
-    sex (value) {
+    }
+  }
+}
+
+export const sex = {
+  filters: {
+    sexByInitial (value) {
 
       return [{initial: 'M', value: 'masculino'}, {initial: 'F', value: 'feminino'}]
         .find((sex) => sex.initial === value).value
-    },
-    date (dateString) {
-      const delivery = new Date(dateString).toLocaleDateString("pt-BR")
-      return delivery !== "Invalid Date" ? delivery : ""
-    },
-    id(healthCenter, attendanceId) {
-      return healthCenter + '/' + attendanceId
+    }
+  }
+}
+
+export const uppercase = {
+  filters: {
+    toUpperCase(value) {
+      return value.toUpperCase()
+    }
+  }
+}
+
+export const date = {
+  filters: {
+    dateFormat (dateString) {
+      if (dateString) {
+        const date = new Date(dateString).toLocaleDateString("pt-BR")
+        return date !== "Invalid Date" ? date : ""
+      } 
+      return "" 
+    }
+  }
+}
+
+export const attendance = {
+  filters: {
+    id (healthCenter, attendance) {
+      let hc = healthCenter
+      let att = attendance
+
+      if (healthCenter < 10) {
+        hc = '00' + healthCenter
+      }
+      if (healthCenter > 9) {
+        hc = '0' + healthCenter
+      }
+      if (attendance < 10) {
+        att = '0000' + attendance
+      }
+      if (attendance > 9) {
+        att = '000' + attendance
+      }
+      if (attendance > 99) {
+        att = '00' + attendance
+      }
+      if (attendance > 999) {
+        att = '0' + attendance
+      }
+      
+      return hc + '/' + att
+    }
+  }
+}
+
+export const phone = {
+  filters: {
+    phoneTrim (value) {
+      return value.replace(/\s/g, '')
+    }
+  }
+}
+
+export const correlative = {
+  filters: {
+    correl(value) {
+      if (value < 10) {
+        return '0' + value
+      }
+      return value
     }
   }
 }
