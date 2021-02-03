68cd9a8ddf627995a97b60becefa157251b10f5a
rodrigoRolim <rodrigorolimveras92@gmail.com>
2021-02-03 15:16:40 -0300

    add new messages to user

--- src/utils/statusMessages.js
+++ src/utils/statusMessages.js
@@ -3,9 +3,10 @@ import {
   MESSAGE_STATUS_502, 
   MESSAGE_STATUS_401, 
   MESSAGE_STATUS_404,
-  MESSAGE_STATUS_400
+  MESSAGE_STATUS_400,
+  MESSAGE_STATUS_403
 } from './alias'
-export const httpMessage = ({status, typeMessage= 'default', data = 'recurso', experied = false}) => {
+export const httpMessage = ({status, typeMessage= 'default', data = 'recurso' }) => {
   console.log(status)
   switch (status) {
     case 111:
@@ -13,12 +14,14 @@ export const httpMessage = ({status, typeMessage= 'default', data = 'recurso', e
     case 404:
       return { type: typeMessage, message: MESSAGE_STATUS_404(data) }
     case 401: 
-      return { type: 'error', message: (experied) ? MESSAGE_STATUS_401 : '' }
+      return { type: 'error', message: MESSAGE_STATUS_401 }
     case 502:
-      return { type: 'error', message: MESSAGE_STATUS_502}
+      return { type: 'error', message: MESSAGE_STATUS_502 }
     case 400:
       return { type: 'error', message: MESSAGE_STATUS_400 }
     case 500: 
-      return { type: 'error', message: 'error no servidor'}
+      return { type: 'error', message: 'error no servidor' }
+    case 403:
+      return { type: 'error', message: MESSAGE_STATUS_403 }
   }
 }
