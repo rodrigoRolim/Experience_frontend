a73edfb50a98f50fd7f5fbc3ad4dd136c047ab8c
rodrigoRolim <rodrigorolimveras92@gmail.com>
2021-03-26 13:50:42 -0300

    refactor paramsQuery

--- src/components/AttendanceListFilterHealthCenter.vue
+++ src/components/AttendanceListFilterHealthCenter.vue
@@ -280,9 +280,9 @@ export default {
     paramsQuery () {
       let queries = {}
       // if (this.params.healthCenter.id) queries['postocadastro'] = this.params.healthCenter.id
-      if (this.params.realizer.id) queries['postorealizante'] = this.params.realizer.id
-      if (this.params.accomodation.id) queries['acomodacao'] = this.params.accomodation.id
-      if (this.params.situation.id) queries['situacao'] = this.params.situation.id
+      if (this.params.realizer) queries['postorealizante'] = this.params.realizer.id
+      if (this.params.accomodation) queries['acomodacao'] = this.params.accomodation.id
+      if (this.params.situation) queries['situacao'] = this.params.situation.id
       queries['limit'] = this.params.limit
       queries['page'] = this.params.page
       return queries
